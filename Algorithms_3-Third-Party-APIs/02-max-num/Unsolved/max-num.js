8a6993d56984477de43741e11c31857f88639a1a
Jerry Guerrero <JerryGDev126@gmail.com>
2022-07-11 15:51:38 -0700

    third group of algorithms solved, pending comments

--- Algorithms_3-Third-Party-APIs/02-max-num/Unsolved/max-num.js
+++ Algorithms_3-Third-Party-APIs/02-max-num/Unsolved/max-num.js
@@ -1,3 +1,11 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+var maxNum = function (arr) {
+  console.log(arr);
+  var sortedArr = arr.sort(function (a, b) {
+    return a - b;
+  });
+  console.log(arr);
+  var lastPlace = sortedArr.length - 1;
+  return sortedArr[lastPlace];
+};
--- Algorithms_3-Third-Party-APIs/03-vowel-count/Unsolved/vowel-count.js
+++ Algorithms_3-Third-Party-APIs/03-vowel-count/Unsolved/vowel-count.js
@@ -1,3 +1,23 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+var vowelCount = function (str) {
+  var vowels = 0;
+  if (str !== "") {
+    console.log(str);
+    var stringArr = str.toLowerCase().split("");
+    console.log(stringArr);
+    for (i = 0; i < stringArr.length; i++) {
+      if (
+        stringArr[i] === "a" ||
+        stringArr[i] === "e" ||
+        stringArr[i] === "i" ||
+        stringArr[i] === "o" ||
+        stringArr[i] === "u"
+      ) {
+        vowels++;
+        console.log(stringArr[i]);
+      }
+    }
+  }
+  return vowels;
+};
