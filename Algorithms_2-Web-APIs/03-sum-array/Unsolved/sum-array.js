0d94594b512f1ea76476fec978f9c6538f80ffba
Jerry Guerrero <JerryGDev126@gmail.com>
2022-07-11 14:55:33 -0700

    Second group of algorithms done, pending comments

--- Algorithms_2-Web-APIs/03-sum-array/Unsolved/sum-array.js
+++ Algorithms_2-Web-APIs/03-sum-array/Unsolved/sum-array.js
@@ -1,3 +1,10 @@
 // Write code to add all the numbers in `arr` and return the total
 
-var sumArray = function(arr) {};
+var sumArray = function (arr) {
+  var result = 0;
+  for (i = 0; i < arr.length; i++) {
+    result += arr[i];
+    console.log(result);
+  }
+  return result;
+};
