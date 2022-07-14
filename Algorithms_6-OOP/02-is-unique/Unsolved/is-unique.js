4b411df7428e9266342ec995094fe4ac40991e56
Jerry Guerrero <JerryGDev126@gmail.com>
2022-07-14 09:59:33 -0700

    sixth group finished, pending comments

--- Algorithms_6-OOP/02-is-unique/Unsolved/is-unique.js
+++ Algorithms_6-OOP/02-is-unique/Unsolved/is-unique.js
@@ -1,4 +1,12 @@
 // Write code to create a function that accepts an array numbers
 // Return `true` is no number appears in the array more than once, else return `false`
 
-var isUnique = function(arr) {};
+var isUnique = function (arr) {
+  if (arr.length < 1) {
+    return true;
+  }
+  if (arr.length === new Set(arr).size) {
+    return true;
+  }
+  return false;
+};
--- Algorithms_6-OOP/03-is-anagram/Unsolved/is-anagram.js
+++ Algorithms_6-OOP/03-is-anagram/Unsolved/is-anagram.js
@@ -1,4 +1,9 @@
 // Write code to create a function that accepts two strings
 // Return true if the strings are anagrams of each other, otherwise return false
 
-var isAnagram = function(strA, strB) {};
+var isAnagram = function (strA, strB) {
+  if (strA.split("").sort().join("") === strB.split("").sort().join("")) {
+    return true;
+  }
+  return false;
+};
