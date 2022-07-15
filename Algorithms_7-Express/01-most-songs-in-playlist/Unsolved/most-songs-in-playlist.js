cbc00d87f8418044bc997ba733623f36601560b2
Jerry Guerrero <JerryGDev126@gmail.com>
2022-07-14 19:14:33 -0700

    seventh group done! pending comments

--- Algorithms_7-Express/01-most-songs-in-playlist/Unsolved/most-songs-in-playlist.js
+++ Algorithms_7-Express/01-most-songs-in-playlist/Unsolved/most-songs-in-playlist.js
@@ -2,6 +2,24 @@
 // Each number represents the duration of a song in minutes
 // Return the maximum number of songs from the list that can fit in a 60 minute playlist
 
-var mostSongsInPlaylist = function(arr) {
-  // add your code here...
+var mostSongsInPlaylist = function (arr) {
+  if (arr.length < 1) {
+    return 0;
+  }
+  var sum = 0;
+  var sortedArr = arr.sort(function (a, b) {
+    return a - b;
+  });
+  for (i = 0; i <= sortedArr.length; i++) {
+    if (sum > 60) {
+      return i - 1;
+    }
+    if (i === sortedArr.length) {
+      return i;
+    }
+    console.log(i);
+    console.log(sum);
+
+    sum += arr[i];
+  }
 };
