07e7ec3128758aad8cf8a2a6000575cf90225a6a
Jerry Guerrero <JerryGDev126@gmail.com>
2022-07-20 15:00:33 -0700

    group 8 done! pending comments!

--- Algorithms_8-SQL/01-multiply-into-20/Unsolved/multiply-into-20.js
+++ Algorithms_8-SQL/01-multiply-into-20/Unsolved/multiply-into-20.js
@@ -1,3 +1,11 @@
-const multiplyInto20 = function(arr) {
-  // TODO: Write Your Code Here
+const multiplyInto20 = function (arr) {
+  var bool = false;
+  arr.forEach((element) => {
+    for (i = 0; i < arr.length; i++) {
+      if (element * arr[i] === 20) {
+        return (bool = true);
+      }
+    }
+  });
+  return bool;
 };
--- Algorithms_8-SQL/02-zeroes-and-ones/Unsolved/zeroes-and-ones.js
+++ Algorithms_8-SQL/02-zeroes-and-ones/Unsolved/zeroes-and-ones.js
@@ -2,4 +2,20 @@
 // Return true if there are an equal number of 0s and 1s
 // Else return false
 
-var zeroesAndOnes = function(str) {};
+var zeroesAndOnes = function (str) {
+  var splitStr = str.split("");
+  var zeroesCount = 0;
+  var onesCount = 0;
+  for (i = 0; i < splitStr.length; i++) {
+    if (splitStr[i] === "0") {
+      zeroesCount++;
+    }
+    if (splitStr[i] === "1") {
+      onesCount++;
+    }
+  }
+  if (zeroesCount === onesCount) {
+    return true;
+  }
+  return false;
+};
