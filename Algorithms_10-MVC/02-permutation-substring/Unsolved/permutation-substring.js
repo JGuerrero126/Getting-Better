54229868370230f6671c39d5030746baa2ad7aad
Jerry Guerrero <JerryGDev126@gmail.com>
2022-07-20 18:32:53 -0700

    GROUP 10 DONE! no comments yet

--- Algorithms_10-MVC/02-permutation-substring/Unsolved/permutation-substring.js
+++ Algorithms_10-MVC/02-permutation-substring/Unsolved/permutation-substring.js
@@ -2,4 +2,20 @@
 // Return true if the second string is a substring of any permutation of the first string
 // Else return false
 
-var permutationSubstring = function(str, sub) {};
+var permutationSubstring = function (str, sub) {
+  var sortedStr = [...new Set(str.split("").sort())];
+  var sortedSub = sub.split("").sort();
+  var matchCount = 0;
+
+  sortedSub.forEach((element) => {
+    for (i = 0; i < sortedStr.length; i++) {
+      if (element === sortedStr[i]) {
+        matchCount++;
+      }
+    }
+  });
+  if (matchCount === sortedSub.length) {
+    return true;
+  }
+  return false;
+};
--- Algorithms_10-MVC/03-concert-flyer/Unsolved/concert-flyer.js
+++ Algorithms_10-MVC/03-concert-flyer/Unsolved/concert-flyer.js
@@ -1,3 +1,23 @@
 // Write a function that takes two strings and returns true if every word found in the second string is present in the first string. You will be checking for both words and their frequency. Assume you'll need to worry about casing, but the strings won't contain any punctuation. Assume neither string will be empty
 
-var concertFlyer = function (magazine, flyer) {};
+var concertFlyer = function (magazine, flyer) {
+  console.log(magazine.split(" ").sort());
+  console.log(flyer.split(" ").sort());
+
+  var sortedMag = magazine.split(" ");
+  var sortedFly = flyer.split(" ");
+  var matchCount = 0;
+
+  sortedFly.forEach((element) => {
+    for (i = 0; i < sortedMag.length; i++) {
+      if (element === sortedMag[i]) {
+        console.log("MATCH FOUND");
+        matchCount++;
+      }
+    }
+  });
+  if (matchCount === sortedFly.length) {
+    return true;
+  }
+  return false;
+};
