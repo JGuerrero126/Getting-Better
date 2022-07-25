b8db9fe237e505e7d5624af8f248925db32b8863
Jerry Guerrero <JerryGDev126@gmail.com>
2022-07-25 13:09:06 -0700

    11th group done! no comments yet!

--- Algorithms_11-CS/01-reverse-no-built-in/Unsolved/reverse-no-built-in.js
+++ Algorithms_11-CS/01-reverse-no-built-in/Unsolved/reverse-no-built-in.js
@@ -1,4 +1,12 @@
 // Write code to return a new string with all of the characters in `str` reversed.
 // You may NOT use the built-in reverse method
 
-const reverse = (str) => {};
+const reverse = (str) => {
+  var splitStr = str.split("");
+  var revStr = [];
+  splitStr.forEach((element) => {
+    revStr.unshift(element);
+  });
+
+  return revStr.join("");
+};
--- Algorithms_11-CS/03-reverse-in-place/Unsolved/reverse-in-place.js
+++ Algorithms_11-CS/03-reverse-in-place/Unsolved/reverse-in-place.js
@@ -1,4 +1,14 @@
 // Write code to return a new string with all of the characters in `str` reversed.
 // You may NOT use the built-in reverse method
 
-const reverseInPlace = (arr) => {};
+const reverseInPlace = (arr) => {
+  var endPoint = arr.length - 1;
+  var startPoint = 0;
+
+  while (startPoint < endPoint) {
+    var takeEnd = arr.splice(endPoint, 1);
+    var moveToPlace = arr.splice(startPoint, 0, parseInt(takeEnd.toString()));
+    startPoint++;
+  }
+  return arr;
+};
